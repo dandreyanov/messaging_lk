5f1affacda3a73a047ebe15f816ca05c99066d02
Denis Andreyanov <andreyanov.denis92@gmail.com>
2021-03-28 20:05:56 +0300

    remove unused method, changed xpath selectors

--- src/test/java/helpers/PageHelper.java
+++ src/test/java/helpers/PageHelper.java
@@ -1,20 +1,15 @@
 package helpers;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
 
 public class PageHelper {
-    public static void openPersonalData() {
-        $x("//span[contains(text(),'Профиль')]").click();
-        $x("//a[contains(text(),'Личные данные')]").click();
-    }
 
     public static void clickSave() {
-        $x("//button[contains(text(),'Сохранить')]").click();
+        $("button.btn._green").click();
     }
 
     public static void clickChange() {
-        $x("//button[contains(text(),'Изменить')]").click();
+        $("button.btn._blue").click();
     }
 
     public static void setProfileData(String s, String userData) {
